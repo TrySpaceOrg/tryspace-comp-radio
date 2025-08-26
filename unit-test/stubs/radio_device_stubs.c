3750a43a42fc1472a7b794048e942d430718a14b
Lucas, John P. <John.P.Lucas@tryspace.org>
2025-08-26 11:14:14 -0400

    [tryspaceorg/tryspace-lab#28] Updates to unit test stubs;

--- unit-test/stubs/radio_device_stubs.c
+++ unit-test/stubs/radio_device_stubs.c
@@ -1,27 +1,27 @@
 #include "utgenstub.h"
 #include "radio_device.h"
 
-int32_t RADIO_ReceiveData(spi_info_t *device, uint8_t *data, uint8_t max_length, uint8_t *actual_length)
+int32_t RADIO_ReceiveData(spi_info_t *device, uint8_t *data, uint16_t max_length, uint16_t *actual_length)
 {
-    UT_GenStub_SetupReturnBuffer(RADIO_ReadData, int32_t);
+    UT_GenStub_SetupReturnBuffer(RADIO_ReceiveData, int32_t);
 
     UT_GenStub_AddParam(RADIO_ReceiveData, spi_info_t *, device);
     UT_GenStub_AddParam(RADIO_ReceiveData, uint8_t *, data);
-    UT_GenStub_AddParam(RADIO_ReceiveData, uint8_t, max_length);
-    UT_GenStub_AddParam(RADIO_ReceiveData, uint8_t *, actual_length);
+    UT_GenStub_AddParam(RADIO_ReceiveData, uint16_t, max_length);
+    UT_GenStub_AddParam(RADIO_ReceiveData, uint16_t *, actual_length);
 
     UT_GenStub_Execute(RADIO_ReceiveData, Basic, NULL);
 
     return UT_GenStub_GetReturnValue(RADIO_ReceiveData, int32_t);
 }
 
-int32_t RADIO_CommandDevice(spi_info_t *device, uint8_t cmd, uint8_t payload_len, uint8_t *payload)
+int32_t RADIO_CommandDevice(spi_info_t *device, uint8_t cmd, uint16_t payload_len, uint8_t *payload)
 {
     UT_GenStub_SetupReturnBuffer(RADIO_CommandDevice, int32_t);
 
     UT_GenStub_AddParam(RADIO_CommandDevice, spi_info_t *, device);
     UT_GenStub_AddParam(RADIO_CommandDevice, uint8_t, cmd);
-    UT_GenStub_AddParam(RADIO_CommandDevice, uint8_t, payload_len);
+    UT_GenStub_AddParam(RADIO_CommandDevice, uint16_t, payload_len);
     UT_GenStub_AddParam(RADIO_CommandDevice, uint8_t *, payload);
 
     UT_GenStub_Execute(RADIO_CommandDevice, Basic, NULL);
